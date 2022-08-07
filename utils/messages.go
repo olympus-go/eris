c61e5a2d6e5c80c63dce413c0c282b3dad76dca6
Eric <ericolsonnv@gmail.com>
2022-08-07 07:36:08 -0700

    Remove unused MessageComponentBuilder

--- utils/messages.go
+++ utils/messages.go
@@ -10,10 +10,6 @@ type ActionsRowBuilder struct {
 	messageComponents []discordgo.MessageComponent
 }
 
-type MessageComponentBuilder struct {
-	messageComponents []discordgo.MessageComponent
-}
-
 func Button() *ButtonBuilder {
 	return &ButtonBuilder{
 		button: discordgo.Button{
@@ -82,48 +78,3 @@ func (a *ActionsRowBuilder) Build() discordgo.ActionsRow {
 		Components: a.messageComponents,
 	}
 }
-
-func MessageComponent() *MessageComponentBuilder {
-	return &MessageComponentBuilder{}
-}
-
-func (m *MessageComponentBuilder) Button(style discordgo.ButtonStyle, message string, id string) *MessageComponentBuilder {
-	button := discordgo.Button{
-		Style:    style,
-		Label:    message,
-		CustomID: id,
-	}
-
-	m.messageComponents = append(m.messageComponents, button)
-	return m
-}
-
-func (m *MessageComponentBuilder) Value() []discordgo.MessageComponent {
-	return m.messageComponents
-}
-
-//		Components: []discordgo.MessageComponent{
-//			discordgo.ActionsRow{
-//				Components: []discordgo.MessageComponent{
-//					discordgo.Button{
-//						Label:    "Characters",
-//						Style:    discordgo.PrimaryButton,
-//						CustomID: "21q_theme_characters",
-//					},
-//					discordgo.Button{
-//						Label:    "Animals",
-//						Style:    discordgo.PrimaryButton,
-//						CustomID: "21q_theme_animals",
-//					},
-//					discordgo.Button{
-//						Label:    "Objects",
-//						Style:    discordgo.PrimaryButton,
-//						CustomID: "21q_theme_objects",
-//					},
-//				},
-//			},
-//		},
-//	}
-//
-//	return &responseData
-//}
