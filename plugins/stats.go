a8bc852fd57eb89b92f6644eac7420d224e1083c
Eric <ericolsonnv@gmail.com>
2022-08-09 22:04:56 -0700

    Update stats plugin to accept additional info

--- plugins/stats.go
+++ plugins/stats.go
@@ -4,43 +4,53 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/olympus-go/eris/utils"
+	"github.com/rs/zerolog"
 	"runtime"
 	"time"
 )
 
-const statusString = `Uptime: %s
-Runtime: %s
-Version: %s`
-
 type StatsPlugin struct {
-	startTime time.Time
-	runtime   string
-}
-
-func Stats() StatsPlugin {
-	return StatsPlugin{
-		startTime: time.Now(),
-		runtime:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	data []struct {
+		key   string
+		value func() string
 	}
+	logger zerolog.Logger
 }
 
-func (s StatsPlugin) Name() string {
+func Stats(logger zerolog.Logger) *StatsPlugin {
+	plugin := StatsPlugin{
+		logger: logger.With().Str("plugin", "stats").Logger(),
+	}
+
+	startTime := time.Now()
+	plugin.AddStatFunc("Uptime", func() string { return time.Since(startTime).Round(time.Second).String() })
+	plugin.AddStat("Runtime", fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH))
+
+	return &plugin
+}
+
+func (s *StatsPlugin) Name() string {
 	return "Stats"
 }
 
-func (s StatsPlugin) Description() string {
+func (s *StatsPlugin) Description() string {
 	return "Prints out various technical stats"
 }
 
-func (s StatsPlugin) Handlers() map[string]any {
+func (s *StatsPlugin) Handlers() map[string]any {
 	handlers := make(map[string]any)
 
 	handlers["stats_handler"] = func(session *discordgo.Session, i *discordgo.InteractionCreate) {
 		switch i.Type {
 		case discordgo.InteractionApplicationCommand:
 			if i.ApplicationCommandData().Name == "stats" {
-				message := fmt.Sprintf(statusString, time.Since(s.startTime).Round(time.Second).String(), s.runtime, "v0.0.1")
-				_ = utils.SendEphemeralInteractionResponse(session, i.Interaction, "```"+message+"```")
+				message := ""
+				for _, stat := range s.data {
+					message += fmt.Sprintf("%s: %s\n", stat.key, stat.value())
+				}
+
+				utils.InteractionResponse(session, i.Interaction).Ephemeral().
+					Message("```" + message + "```").SendWithLog(s.logger)
 			}
 		}
 	}
@@ -48,7 +58,7 @@ func (s StatsPlugin) Handlers() map[string]any {
 	return handlers
 }
 
-func (s StatsPlugin) Commands() map[string]*discordgo.ApplicationCommand {
+func (s *StatsPlugin) Commands() map[string]*discordgo.ApplicationCommand {
 	commands := make(map[string]*discordgo.ApplicationCommand)
 
 	commands["stats_command"] = &discordgo.ApplicationCommand{
@@ -59,6 +69,30 @@ func (s StatsPlugin) Commands() map[string]*discordgo.ApplicationCommand {
 	return commands
 }
 
-func (s StatsPlugin) Intents() []discordgo.Intent {
+func (s *StatsPlugin) Intents() []discordgo.Intent {
 	return nil
+}
+
+func (s *StatsPlugin) AddStat(key string, value string) {
+	kv := struct {
+		key   string
+		value func() string
+	}{
+		key,
+		func() string { return value },
+	}
+
+	s.data = append(s.data, kv)
+}
+
+func (s *StatsPlugin) AddStatFunc(key string, value func() string) {
+	kv := struct {
+		key   string
+		value func() string
+	}{
+		key,
+		value,
+	}
+
+	s.data = append(s.data, kv)
 }
